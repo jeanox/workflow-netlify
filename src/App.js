e5f11d175b99ab94179bef18fa0062fe3c32fcb8
jeanox <beckypennock@gmail.com>
2023-05-05 08:56:14 -0700

    Replacing deets with my face, yay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Biki initializes new workflow!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://github.com/jeanox"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit biki's github
         </a>
       </header>
     </div>
